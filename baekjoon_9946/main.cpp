bc8e1b72be0f9945514bdd0668bd5117b6f16a5d
skeleton625 <chaos0824@naver.com>
2018-08-31 23:54:29 +0900

    9946 문제 풀이 개선중

--- baekjoon_9946/main.cpp
+++ baekjoon_9946/main.cpp
@@ -1,20 +1,17 @@
 #include <cstdio>
-#include <string.h>
 
 int al1[26], al2[26], c, t = 1;
 char i1[1001], i2[1001];
 int main() {
-	while (scanf_s("%s", i1, 1001) && scanf_s("%s", i2, 1001)) {
-		if (strcmp(i1, "END") == 0 && strcmp(i2, "END") == 0) break;
+	while (gets_s(i1,1001)&&gets_s(i2,1001)){
+		if (i1[0] < 'a') break;
 		for (int i = 0; i1[i]; i++) al1[i1[i] - 'a']++;
 		for (int i = 0; i2[i]; i++) al2[i2[i] - 'a']++;
 		for (int i = 0; i < 26; i++) {
 			if (al1[i] == al2[i])c++;
-			al1[i] = 0; al2[i] = 0;
+			al1[i] = al2[i] = 0;
 		}
-		printf("Case %d: ", t++);
-		if (c == 26) printf("same\n");
-		else printf("different\n");
+		printf("Case %d: %s\n", t++, c == 26 ? "same" : "different");
 		c = 0;
 	}
 	return 0;
