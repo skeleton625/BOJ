2404a7124da88d7f1a605b5343ae40a36607f28a
WaniFi <skelpd24@gmail.com>
2018-09-05 00:19:44 +0900

    10824 문제 풀이 완료

---
+++ baekjoon_10824/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+char a[15], b[8], c[15], d[8], r[15];
+int s1, s2, p, m;
+int main() {
+	cin >> a >> b >> c >> d;
+	s1 = strlen(a); s2 = strlen(c);
+	for (int i = s1; i < s1+strlen(b); i++) a[i] = b[i - s1];
+	for (int i = s2; i < s2+strlen(d); i++) c[i] = d[i - s2];
+	cout << atoll(a) + atoll(c);
+	return 0;
+}
