333797cf8d6f740c8a45236584ac713df7500b54
WaniFi <skelpd24@gmail.com>
2018-09-08 14:29:54 +0900

    10990 문제 개선 완료

--- baekjoon_10990/main.cpp
+++ baekjoon_10990/main.cpp
@@ -1,15 +1,10 @@
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
 int t;
 int main() {
-	cin >> t;
-	for (int i = 0; i < t; i++) {
-		for (int j = t - i - 1; j > 0; j--) cout << " ";
-		if (i != 0) cout << "*";
-		else cout << "*"<<endl;
-		for (int j = 0; j < 2*i-1; j++) cout << " ";
-		if (i != 0) cout << "*"<<endl;
-	}
+	scanf_s("%d", &t);
+	for (int i = 0; i < t; ++i && printf("\n"))
+		for (int j = 1; j <= t + i; j++)
+			printf("%c", (j == t - i) || (j == t + i) ? '*' : ' ');
 	return 0;
 }
