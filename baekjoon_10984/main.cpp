dc57a90330837736652ed0e71912e60bceafcdab
WaniFi <skelpd24@gmail.com>
2018-09-08 15:02:32 +0900

    10984 문제 풀이 완료

---
+++ baekjoon_10984/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int t, s, c, m;
+double g, n;
+int main(){
+	scanf_s("%d", &t);
+	while (t--) {
+		scanf_s("%d", &s);
+		for (int i = 0; i < s; i++) {
+			scanf_s("%d %lf", &c, &g);
+			m += c; n += c*g;
+		}
+		printf("%d %.1lf\n", m, n / m);
+	}
+	return 0;
+}
