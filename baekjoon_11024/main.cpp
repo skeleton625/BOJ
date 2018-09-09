735495bf6a0f79b15af84bbdb67937dd5f7ba309
WaniFi <skelpd24@gmail.com>
2018-09-09 18:25:51 +0900

    11024 문제 풀이 개선 완료

--- baekjoon_11024/main.cpp
+++ baekjoon_11024/main.cpp
@@ -1,24 +1,13 @@
 #include <cstdio>
-#include <cstring>
-#include <cstdlib>
 
-int t, m, c;
-char i1[701], n[6];
+int t, m, n;
 int main() {
-	scanf_s("%d%*c", &t);
+	scanf_s("%d", &t);
 	while (t--) {
-		gets_s(i1, 701);
-		for (int i = 0; i1[i]; i++) {
-			if (i1[i] != ' ') n[c++] = i1[i];
-			else {
-				m += atoi(n);
-				memset(n, 0, 6);
-				c = 0;
-			}
-		}
-		m += atoi(n);
-		memset(n, 0, 6);
-		c = 0;
+		do {
+			scanf_s("%d", &n);
+			m += n;
+		} while (getchar() == ' ');
 		printf("%d\n", m);
 		m = 0;
 	}
