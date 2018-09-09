bb7f8d805eb461768a419bf781787f207a9184b5
WaniFi <skelpd24@gmail.com>
2018-09-09 17:51:54 +0900

    11023 문제 풀이 개선 완료

--- baekjoon_11023/main.cpp
+++ baekjoon_11023/main.cpp
@@ -1,20 +1,8 @@
 #include <cstdio>
-#include <cstring>
-#include <cstdlib>
 
-int m, c;
-char n[701], t[6];
+int m, n;
 int main() {
-	gets_s(n, 701);
-	for (int i = 0; i < strlen(n); i++) {
-		if (n[i] != ' ') t[c++] = n[i];
-		else {
-			c = 0;
-			m += atoi(t);
-			memset(t, 0, 6);
-		}
-	}
-	m += atoi(t);
+	while (~scanf_s("%d", &n)) m += n;
 	printf("%d", m);
 	return 0;
 }
