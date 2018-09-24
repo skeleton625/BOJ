b19a73435c3f478214ba387e23fb8560c7c6e87c
skeleton625 <chaos0824@naver.com>
2018-09-24 23:02:52 +0900

    10420 문제 풀이 완료

---
+++ baekjoon_10420/main.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+int i1, y = 2015, m = 4, d = 1;
+int mon[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+int main() {
+	scanf_s("%d", &i1);
+	while (i1 > mon[m - 1]) {
+		i1 -= mon[m - 1];
+		m++;
+		if (m == 13) { 
+			m= 1; y++;
+			mon[1] = (y%4 == 0 && y%100 != 0) ? 29 : y%400 == 0 ? 29 : 28;
+		}
+	}
+	d += i1;
+	if (d > mon[m - 1]) { d -= mon[m - 1]; m++; }
+	if (m == 13) { m = 1; y++; }
+	printf("%d-%02d-%02d", y, m, d);
+	return 0;
+}
