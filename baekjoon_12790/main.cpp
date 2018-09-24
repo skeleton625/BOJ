46f1ed28429363f2a65c71d727659ad0bcda194c
skeleton625 <chaos0824@naver.com>
2018-09-24 23:31:04 +0900

    12790 문제 반복문 사용 안함

--- baekjoon_12790/main.cpp
+++ baekjoon_12790/main.cpp
@@ -1,19 +1,13 @@
 #include <cstdio>
 
 int t, i1;
-int n[4];
+int n[8];
 int main() {
 	scanf_s("%d", &t);
 	while (t--) {
-		for (int i = 0; i < 4; i++) scanf_s("%d", &n[i]);
-		for (int i = 0; i < 4; i++) { 
-			scanf_s("%d", &i1); 
-			n[i] += i1; 
-		}
-		for (int i = 0; i < 2; i++) {
-			if (n[i] < 1) n[i] = 1;
-		}
-		if (n[2] < 0) n[2] = 0;
+		scanf_s("%d%d%d%d%d%d%d%d", &n[0], &n[1], &n[2], &n[3], &n[4], &n[5], &n[6], &n[7]);
+		n[0] += n[4]; n[1] += n[5]; n[2] += n[6]; n[3] += n[7];
+		n[0] <= 0 ? 1 : n[0]; n[1] <= 0 ? 1 : n[1]; n[2] <= 0 ? 0 : n[2];
 		printf("%d\n", n[0] + 5 * n[1] + 2 * n[2] + 2 * n[3]);
 	}
 	return 0;
