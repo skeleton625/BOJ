f6f44cf3a8c784279050714fe165491757cf92d2
WaniFi <skelpd24@gmail.com>
2018-09-29 12:55:14 +0900

    13300 문제 논리 연산자 개선

--- baekjoon_13300/main.cpp
+++ baekjoon_13300/main.cpp
@@ -1,16 +1,16 @@
 #include <cstdio>
 
-int num[2][7];
-int t, rm, ans, s, n, tmp_c;
+int num[2][6];
+int t, rm, ans, s, n;
 int main() {
 	scanf_s("%d %d", &t, &rm);
 	while (t--) {
 		scanf_s("%d %d", &s, &n);
-		num[s][n]++;
+		++num[s][n - 1];
 	}
-	for (int i = 1; i < 7; i++) {
-		ans += num[0][i] / rm + (num[0][i]%rm==0? 0:1);
-		ans += num[1][i] / rm + (num[1][i]%rm==0? 0 : 1);
+	for (int i = 0; i < 6; i++) {
+		ans += (num[0][i]+rm-1) / rm;
+		ans += (num[1][i]+rm-1) / rm;
 	}
 	printf("%d", ans);
 	return 0;
