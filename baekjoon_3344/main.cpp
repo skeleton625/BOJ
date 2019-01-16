c7a8794bb16c5f0623c114a535ea7c5b3535da90
skeleton625 <chaos0824@naver.com>
2019-01-16 15:06:53 +0900

    3344 문제 풀이 중 런타임 에러 발생

--- baekjoon_3344/main.cpp
+++ baekjoon_3344/main.cpp
@@ -1,47 +1,40 @@
 #include <cstdio>
+#include <cstring>
 
-int n, q;
-int *queen;
-
-bool promising(int floor) {
-	for (int i = 0; i < floor; i++) {
-		if (queen[i] - queen[floor] == 0 ||
-			queen[i] - queen[floor] == floor - i ||
-			queen[i] - queen[floor] == i - floor)
-			return false;
-	}
-	return true;
-}
+bool *queen_center, *queen_right, *queen_left;
+int n;
 
 bool find_queen(int floor) {
-	if (!promising(floor-1)) {
-		return false;
-	} else if (floor == n) {
+	if (floor == n) {
 		return true;
-	} else {
-		for (int i = 0; i < n; i++) {
-			if (q >= n) q = 0;
-			queen[floor] = q;
-			q++;
+	}
+	for (int i = 0; i < n; i++) {
+		if (!(queen_center[i] || queen_right[floor + i] || queen_left[n - 1 + floor - i])) {
+			queen_center[i] = queen_right[floor + i] = queen_left[n - 1 + floor - i] = true;
 			if (find_queen(floor + 1)) {
+				printf("%d\n", i + 1);
 				return true;
 			}
+			else {
+				queen_center[i] = queen_right[floor + i] = queen_left[n - 1 + floor - i] = false;
+			}
 		}
-		return false;
+		
 	}
-}
-
-void print_queen() {
-	for (int i = 0; i < n; i++)
-		printf("%d\n", queen[i]+1);
+	return false;
 }
 
 int main() {
 	scanf_s("%d", &n);
-	queen = new int[n];
+	queen_center = new bool[n];
+	queen_right = new bool[n];
+	queen_left = new bool[n];
+
+	memset(queen_center, 0, sizeof(bool)*n);
+	memset(queen_right, 0, sizeof(bool)*(2 * n - 1));
+	memset(queen_left, 0, sizeof(bool)*(2 * n - 1));
 
 	find_queen(0);
-	print_queen();
 
 	return 0;
 }
