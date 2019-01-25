bcec938392309931c4765ead86bae06976a389e7
skeleton625 <chaos0824@naver.com>
2019-01-25 17:41:29 +0900

    6549 문제 풀이 개선(스택 사용)

--- baekjoon_6549/main.cpp
+++ baekjoon_6549/main.cpp
@@ -1,80 +1,41 @@
 #include <iostream>
-#include <cmath>
-#include <vector>
-#include <algorithm>
+#include <stack>
 using namespace std;
 
 typedef unsigned long long ll;
-int nsize;
-
-void init(vector<int> &arr, vector<int> &tree, int index, int start, int end) {
-	if (start == end)
-		tree[index] = start;
-	else {
-		int mid = (start + end) / 2;
-		init(arr, tree, index * 2, start, mid);
-		init(arr, tree, index * 2 + 1, mid + 1, end);
-		if (arr[tree[index * 2]] <= arr[tree[index * 2 + 1]])
-			tree[index] = tree[index * 2];
-		else
-			tree[index] = tree[index * 2 + 1];
-	}
-}
-
-int query(vector<int> &arr, vector<int> &tree, int index, int start, int end, int left, int right) {
-	if (left > end || right < start) {
-		return -1;
-	}
-	if (left <= start && end <= right) {
-		return tree[index];
-	}
-	else {
-		int mid = (start + end) / 2;
-		int m1 = query(arr, tree, index * 2, start, mid, left, right);
-		int m2 = query(arr, tree, index * 2 + 1, mid + 1, end, left, right);
-		
-		if (m1 == -1) {
-			return m2;
-		} else if (m2 == -1) {
-			return m1;
-		} else {
-			if (arr[m1] <= arr[m2]) {
-				return m1;
-			}
-			else
-				return m2;
-		}
-	}
-}
-
-unsigned long long getMax(vector<int> &arr, vector<int> &tree, int start, int end) {
-	int min_pos = query(arr, tree, 1, 0, nsize - 1, start, end);
-	ll pre_max = (ll)arr[min_pos] * (ll)(end - start + 1);
-
-	if (start <= min_pos - 1) {
-		ll left = getMax(arr, tree, start, min_pos - 1);
-		if (pre_max < left) pre_max = left;
-	}
-	if (min_pos + 1 <= end) {
-		ll right = getMax(arr, tree, min_pos + 1, end);
-		if (pre_max < right) pre_max = right;
-	}
-	return pre_max;
-}
+int h[100000], hsize;
 
 int main() {
 	cin.tie(0);
 	ios::sync_with_stdio(false);
+	int width, height;
+	ll maxi;
 	while (1) {
-		cin >> nsize;
-		if (nsize == 0) break;
-		int h = ceil(log2(nsize));
-		vector<int> n(nsize);
-		vector<int> tree(1 << (h + 1));
-		for (int i = 0; i < nsize; i++)
-			cin >> n[i];
-		init(n, tree, 1, 0, nsize - 1);
-		cout << getMax(n, tree, 0, nsize - 1) << '\n';
+		cin >> hsize;
+		if (hsize == 0) break;
+		maxi = 0;
+		for (int i = 0; i < hsize; i++) cin >> h[i];
+		stack<int> s;
+		for (int i = 0; i < hsize; i++) {
+			while (!s.empty() && h[s.top()] > h[i]) {
+				height = h[s.top()];
+				s.pop();
+				if (!s.empty()) width = i - s.top() - 1;
+				else width = i;
+				if ((ll)width*(ll)height > maxi) maxi = (ll)width * (ll)height;
+			}
+			s.push(i);
+		}
+
+		while (!s.empty()) {
+			height = h[s.top()];
+			s.pop();
+			if (!s.empty()) width = hsize - s.top() - 1;
+			else width = hsize;
+			if ((ll)width*(ll)height > maxi) 
+				maxi = (ll)width * (ll)height;
+		}
+		cout << maxi << '\n';
 	}
 	return 0;
 }
