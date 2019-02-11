b48b35f31dee35e2f0368f620cdf716064741a82
skeleton625 <chaos0824@naver.com>
2019-02-11 14:30:39 +0900

    1927 문제 풀이 완료(우선순위 큐 사용)

--- baekjoon_1927/main.cpp
+++ baekjoon_1927/main.cpp
@@ -1,59 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
+#include <queue>
 using namespace std;
 
-int n, c;
-vector<int> mat;
-
-void input_heap() {
-	int chi = c - 1, par = (chi-1)/2;
-	while (chi > 0) {
-		if (mat[par] > mat[chi]) swap(mat[par], mat[chi]);
-		chi = par;
-		par = (chi - 1) / 2;
-	}
-}
-
-void delete_heap(int par) {
-	int left = 2*par+1, right = 2*par+2;
-	if (mat[left] > mat[right] && mat[right] != 0) {
-		if (mat[par] > mat[right]) {
-			swap(mat[par], mat[right]);
-			delete_heap(right);
-		}
-	} else if(mat[left] != 0){
-		if (mat[par] > mat[left]) {
-			swap(mat[par], mat[left]);
-			delete_heap(left);
-		}
-	}
-}
+int n;
+priority_queue<int> que;
 
 int main() {
 	int num;
 	cin.tie(0);
 	ios::sync_with_stdio(false);
-
+	
 	cin >> n;
-	int len = ceil(log2(n));
-	mat = vector<int>(1 << len, 0);
 	while (n--) {
 		cin >> num;
 		if (num == 0) {
-			if(c == 0) cout << 0 << '\n';
+			if (que.empty()) cout << 0 << '\n';
 			else {
-				cout << mat[0] << '\n';
-				mat[0] = mat[c - 1];
-				mat[c - 1] = 0;
-				delete_heap(0);
-				c--;
+				cout << -que.top() << '\n';
+				que.pop();
 			}
-		} else {
-			mat[c] = num;
-			c++;
-			input_heap();
 		}
+		else que.push(-num);
 	}
 
 	return 0;
